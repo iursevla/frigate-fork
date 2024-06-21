65e8a02f5604a3deb335410ed479770c9d088825
Jason Hunter <hunterjm@gmail.com>
2024-06-21 17:30:19 -0400

    Semantic Search for Detections (#11899)
    * Initial re-implementation of semantic search
    
    * put docker-compose back and make reindex match docs
    
    * remove debug code and fix import
    
    * fix docs
    
    * manually build pysqlite3 as binaries are only available for x86-64
    
    * update comment in build_pysqlite3.sh
    
    * only embed objects
    
    * better error handling when genai fails
    
    * ask ollama to pull requested model at startup
    
    * update ollama docs
    
    * address some PR review comments
    
    * fix lint
    
    * use IPC to write description, update docs for reindex
    
    * remove gemini-pro-vision from docs as it will be unavailable soon
    
    * fix OpenAI doc available models
    
    * fix api error in gemini and metadata for embeddings

--- frigate/events/cleanup.py
+++ frigate/events/cleanup.py
@@ -27,6 +27,7 @@ class EventCleanup(threading.Thread):
         self.name = "event_cleanup"
         self.config = config
         self.stop_event = stop_event
+        self.embeddings = Embeddings()
         self.camera_keys = list(self.config.cameras.keys())
         self.removed_camera_labels: list[str] = None
         self.camera_labels: dict[str, dict[str, any]] = {}
